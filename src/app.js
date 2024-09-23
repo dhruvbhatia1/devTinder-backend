d23f156ba0ee55dbff3aa0c6891652043af901d4
dhruvbhatia1 <dbhatia03@gmail.com>
2024-09-23 17:34:15 +0530

    build basic APIs

--- src/app.js
+++ src/app.js
@@ -2,23 +2,74 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
 const User = require("./models/user");
+app.use(express.json()); // middleware to parse the incoming request body
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 app.post("/signup", async (req, res, next) => {
-  const userObj = {
-    firstName: "Dhruv",
-    lastName: "Bhatia",
-    email: "db@gmail.com",
-    password: "password",
-    age: 21,
-  };
+  //   console.log(req.body);
 
-  const user = new User(userObj);
+  const user = new User(req.body);
 
   try {
     const result = await user.save();
     res.send(result);
   } catch (err) {
     res.status(400).send(err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const result = await User.findByIdAndDelete(userId);
+    if (!result) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(result);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  try {
+    const result = await User.findByIdAndUpdate(data.userId, data);
+    if (!result) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(result);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
